15b55197e73fff7506431bde9fc4d088313a3ce8
ashish ghimire <ashish.gh123@gmail.com>
2020-05-28 11:53:58 +0545

    Set setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="zomatoWrapper",
+    version="0.1",
+    description="Zomato API integration with python",
+    url="https://github.com/ashish-gh/zomatoWrapper",
+    author="Ashish Ghimire",
+    author_email="ashish.gh123@gmail.com",
+    license="MIT",
+    packages=["zomatoWrapper"],
+    install_requires=["requests",],
+    zip_safe=False,
+)
---
+++ zomatoWrapper/__init__.py
@@ -0,0 +1,1 @@
+from .zomatoWrapper import Zomato
